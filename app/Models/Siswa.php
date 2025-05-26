e8478c0f623e61dae30784a3186582593ad85caa
ranggo pato <patoranggo@gmail.com>
2025-05-26 10:09:50 +0700

    factory

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Siswa extends Model
 {
+    use HasFactory;
     // Siswa cari ke table siswas
     // User cari ke table users
     protected $table = 'siswa';
